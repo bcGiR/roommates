172eeb3eaaba474be6168a801e2d5f7cc79bc910
Brendan Crawford <bcgir87@gmail.com>
2014-09-26 13:26:50 -0700

    split logout page from logout script

---
+++ logout-exec.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_SESSION['username'];
+$query = "UPDATE `user` SET active=0 WHERE username='$username'";
+$result = mysql_query($query) or die(mysql_error());
+session_destroy();
+?>
--- logout.php
+++ logout.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 require('connect.php');
-$username = $_SESSION['username'];
-$query = "UPDATE `user` SET active=0 WHERE username='$username'";
-$result = mysql_query($query) or die(mysql_error());
-session_destroy();
+require('logout.php');
 include_once('header.php');
 ?>
 <h2>You have logged out successfully</h2>
