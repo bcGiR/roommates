6d6e8ecb5dc2c10ddd2cbc628a10ee43fd32b9e8
Brendan Crawford <bcgir87@gmail.com>
2014-09-26 14:49:42 -0700

    added a redirect to login from signup submit

--- signup-exec.php
+++ signup-exec.php
@@ -9,6 +9,9 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['emai
         $result = mysql_query($query);
         if ($result) {
             $message = "User Created Successfully";
+            echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/login.php'</script>";
+        } else {
+            $message = "Sorry, there was an unexpected error.\nPlease try again.";
             echo "<script type='text/javascript'>alert('$message');</script>";
         }
     }
