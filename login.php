388f5a3f427e3171e5a8e5385fe3e00baab10737
Brendan Crawford <bcgir87@gmail.com>
2014-09-26 13:22:31 -0700

    split up login page from login script

--- login.php
+++ login.php
@@ -2,29 +2,7 @@
     ob_start();
     session_start();
     require('connect.php');
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $query = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
-        $result = mysql_query($query) or die(mysql_error());
-        $count = mysql_num_rows($result);
-        if ($count == 1){
-            $query_active = "UPDATE `user` SET active=1 WHERE username='$username'";
-            $result_active = mysql_query($query_active) or die(mysql_error());
-            session_regenerate_id();
-            $_SESSION['username'] = $username;
-            $_SESSION['password'] = $password;
-            session_write_close();
-            $message = "Welcome $username";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-            header('Location: http://localhost/mates/home.php');
-            exit();
-        }else{
-            $message = "Invalid Login Credentials";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-        }
-    }
+    require('login-exec.php');
     include_once('header.php');
 ?>
 <form action="" method="post">
