f9eada020a2554bfaddcb34325faf3fdb2a39a2d
Brendan Crawford <bcgir87@gmail.com>
2014-09-26 14:45:05 -0700

    moved redirect after login to javascript from php

--- login-exec.php
+++ login-exec.php
@@ -14,9 +14,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         $_SESSION['password'] = $password;
         session_write_close();
         $message = "Welcome $username";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-        header('Location: http://localhost/mates/home.php');
-        exit();
+        echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/home.php';</script>";
     }else{
         $message = "Invalid Login Credentials";
         echo "<script type='text/javascript'>alert('$message');</script>";
