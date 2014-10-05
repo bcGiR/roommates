ac2cd8fb2b677a9b1a9b53a814c24bf24d1e383c
Brendan Crawford <bcgir87@gmail.com>
2014-10-05 13:19:55 -0700

    added isset to fix signup login, fixed other minor issues

--- login-exec.php
+++ login-exec.php
@@ -1,40 +1,42 @@
 <?php
 require('dbadapter.php');
-$verified = true;
-$error = "Login Failed.\n";
+if (isset($_POST['username'])) {
+    $verified = true;
+    $error = "Login Failed. ";
 
-if ($_POST['username'] === "") {
-    $verified = false;
-    $error .= "Username required.\n";
-}
-if ($_POST['password'] === "") {
-    $verified = false;
-    $error .= "Password required.\n";
-}
+    if ($_POST['username'] === "") {
+        $verified = false;
+        $error .= "Username required. ";
+    }
+    if ($_POST['password'] === "") {
+        $verified = false;
+        $error .= "Password required. ";
+    }
 
-if ($verified) {
-    $username = $_POST['username'];
+    if ($verified) {
+        $username = $_POST['username'];
 
-    $result = getUserByName($username);
-    $count = mysql_num_rows($result);
-    if ($count == 1){
-        $result_active = userActive($username);
-        $row = mysql_fetch_row($result);
-        if (password_verify($_POST['password'], $row[3])) {
-            session_regenerate_id();
-            $_SESSION['username'] = $username;
-            session_write_close();
-            $message = "Welcome $username";
-            echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/home.php';</script>";
+        $result = getUserByName($username);
+        $count = mysql_num_rows($result);
+        if ($count === 1){
+            $result_active = userActive($username);
+            $row = mysql_fetch_row($result);
+            if (password_verify($_POST['password'], $row[3])) {
+                session_regenerate_id();
+                $_SESSION['username'] = $username;
+                session_write_close();
+                $message = "Welcome $username";
+                echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/home.php';</script>";
+            } else {
+                $message = "Sorry, the password did not match, please try again.";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+            }
         } else {
-            $message = "Sorry, the password did not match, please try again.";
+            $message = "That username does not exist. Please try again or sign up.";
             echo "<script type='text/javascript'>alert('$message');</script>";
         }
-    }else{
-        $message = "$result";
-        echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/signup.php';</script>";
+    } else {
+        echo "<script type='text/javascript'>alert('$error');</script>";
     }
-} else {
-    echo "<script type='text/javascript'>alert('$error');</script>";
 }
 ?>
--- signup-exec.php
+++ signup-exec.php
@@ -1,22 +1,27 @@
 <?php
 require('dbadapter.php');
-$verified = true;
-$error = "Sign up failed.\n";
+if (isset($_POST['username'])) {
+    $verified = true;
+    $error = "Sign up failed. ";
 
-if ($_POST['username'] === "") {
-    $verified = false;
-    $error .= "Username required.\n";
-}
-if (!($_POST['password'] === $_POST['repeat_password'])) {
-    $verified = false;
-    $error .= "Passwords must match\n";
-}
-if ($_POST['email'] === "") {
-    $verified = false;
-    $error .= "Email required.\n";
-}
+    if ($_POST['username'] === "") {
+        $verified = false;
+        $error .= "Username required. ";
+    }
+    if ($_POST['password'] === "") {
+        $verified = false;
+        $error .= "Password required. ";
+    }
+    if (!($_POST['password'] === $_POST['repeat_password'])) {
+        $verified = false;
+        $error .= "Passwords must match ";
+    }
+    if ($_POST['email'] === "") {
+        $verified = false;
+        $error .= "Email required. ";
+    }
 
-if ($verified) {
+    if ($verified) {
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -26,10 +31,11 @@ if ($verified) {
             $message = "User Created Successfully";
             echo "<script type='text/javascript'>alert('$message'); window.location.href = 'http://localhost/mates/login.php';</script>";
         } else {
-            $message = "Sorry, there was an unexpected error.\nPlease try again.";
+            $message = "Sorry, there was an unexpected error. Please try again.";
             echo "<script type='text/javascript'>alert('$message');</script>";
         }
-} else {
-    echo "<script type='text/javascript'>alert('$error');</script>";
+    } else {
+        echo "<script type='text/javascript'>alert('$error');</script>";
+    }
 }
 ?>
