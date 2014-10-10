4bc5c3b1442fa048e993d60b88a0c36d3c031160
Brendan Crawford <bcgir87@gmail.com>
2014-10-10 12:23:03 -0700

    fixed up admin authentication

--- admin-auth.php
+++ admin-auth.php
@@ -1,9 +1,7 @@
 <?php
 require('dbconfig.php');
 echo "<script type='text/javascript'>var pass;pass=prompt('Enter admin password', '');".
-    "if (pass == $adminPassword) {".
-    "alert('Password ok!');".
-    "} else {".
-    "window.location.href = 'localhost/mates/denied.php';".
+    "if (pass != '$adminPassword') {".
+    "window.location.href = 'http://localhost/mates/denied.php';".
     "}</script>";
 ?>
