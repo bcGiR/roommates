3ba62b93024687838a6094b6a0c76477e3103648
Brendan Crawford <bcgir87@gmail.com>
2014-10-15 13:30:36 -0700

    added dummy view for status.php

---
+++ status.php
@@ -0,0 +1,6 @@
+<?php
+require('auth.php');
+include_once('header.php');
+?>
+<h2>House View</h2>
+<?php include_once('footer.php'); ?>
--- validate.php
+++ validate.php
@@ -89,4 +89,5 @@ function validateCreateHouse() {
         return $msg;
     }
 }
+
 ?>
